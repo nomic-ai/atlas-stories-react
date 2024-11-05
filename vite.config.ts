683c7d8bb39ad52120db6753c96c80306526adca
Ben Schmidt <bmschmidt@gmail.com>
2024-11-05 17:58:40 -0500

    build tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'es2019',
+    minify: 'terser',
+    lib: {
+      entry: __dirname + '/src/main.tsx',
+      // name: 'atlas-stories-react',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDom',
+        },
+      },
+    },
+  },
+});
